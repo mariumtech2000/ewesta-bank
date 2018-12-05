e30a769998e3c318c5e521b228a137458d7cf8d3
mariumtech2000 <38943256+mariumtech2000@users.noreply.github.com>
2018-12-05 11:45:51 +0500

    westa project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function opennav(){
+    document.querySelector('.side-nav').style.display ="block";
+    document.querySelector('.ewesta-head').style.display ='none';
+}
+function closenav(){
+    document.querySelector('.side-nav').style.display = "none";
+    document.querySelector('.ewesta-head').style.display ='block';
+
+}
